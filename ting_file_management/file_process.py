2995e5a3af2bfeaff6c791120b89cc88beb873b4
“brunobatista” <brunobatista101@gmail.com>
2021-01-21 11:26:01 -0300

    testes passando

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,32 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for item in instance:
+        if item["nome_do_arquivo"] == path_file:
+            return
+    result = txt_importer(path_file)
+    returned_object = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(result),
+        "linhas_do_arquivo": result,
+    }
+    instance.enqueue(returned_object)
+    print(returned_object)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    try:
+        item = instance.dequeue()
+        print(f"Arquivo {item['nome_do_arquivo']} removido com sucesso")
+    except IndexError:
+        print("Não há elementos")
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        item = instance.search(position)
+        print(item)
+    except IndexError:
+        print("Posição inválida", file=sys.stderr)
--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,23 @@
+from collections import deque
+
+
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.data = deque()
+
+    def __iter__(self):
+        return iter(self.data)
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self.data.popleft()
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0 or index > len(self.data):
+            raise IndexError
+        return self.data[index]
